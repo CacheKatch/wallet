ec802dcec597333ec372fc0c00cab202642c17c3
CacheKatch <benzaquenhc@gmail.com>
2021-02-05 10:07:04 -0600

    new functions update

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # wallet
+
+# Bit-test derived addresses
+<table>
+  <tr>
+    <td><font size="4">Add No. </font></td>
+    <td><font size="4">Address </font></td>
+    <td><font size="4">Private Key</font></td>
+    <td><font size="4">Public Key </font></td>
+  </tr>
+  <tr>
+    <td><font size="2">1</font></td>
+    <td><font size="2">n24H3Lv9ucMRfRTzqtjdasaqLaEJye6xFp</font></td>
+    <td><font size="2">cNpcyt6bBwsVR5PtBPU3myYzHm8bgJe1dKkqz4BoaAPWEYPF1gQn</font></td>
+    <td><font size="2">027fef5b69b03129c72be2bd913cd7d68ec7aa7dc264abc7dcb61a1424bdb18de7</font></td>
+  </tr>
+  <tr>
+    <td><font size="2">2</font></td>
+    <td><font size="2">mh42D47zZ5sAYBZeUKQw5yc4jPVHq9Kd1g</font></td>
+    <td><font size="2">cNtNRuDWJcJfYxGZ8GCJVjq7xGzQ1gokMkALQuow3Wx3vvs7Z2Az</font></td>
+    <td><font size="2">03470abca7d1c5109cfff70edb3849976229e81b3bb24e398e178537b18459ac5f</font></td>
+  </tr>
+  <tr>
+    <td><font size="2">3</font></td>
+    <td><font size="2">mo5x1hXmBbFPyp8j5Jw4JhWtZhwPuo72xA</font></td>
+    <td><font size="2">cPxACwqy6ENY72Z9a2FAfsiJkpTKEaVZRU4XSJ3XSKa9zu9eeh1g</font></td>
+    <td><font size="2">023455b0f2cdc34acbfa371ecf54b5538ace9dc88f43cb2dbcc2ed1a96542cd645</font></td>
+  </tr>
+  
+</table>
--- wallet.py
+++ wallet.py
@@ -1,11 +1,58 @@
+# import constants
+from constants import BTC
+from constants import ETH
+from constants import BTCTEST
+from constants import LTC
+
+# imports needed for env loads
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# imports needed for mnemonic phrase wallet derive
 import subprocess
 import json
 
-command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
+#imports for bitcoin and ethereum transactions
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from eth_account import Account
+from bit import wif_to_key
+from bit import PrivateKeyTestnet
+from bit import PrivateKey
+from bit import Key
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-output, err = p.communicate()
-p_status = p.wait()
+#imports to broadcast bitcoins and bit-test
+from bit.network import NetworkAPI
 
-keys = json.loads(output)
-print(keys)
+#imports to work in pandas
+import pandas as pd
+
+mnemonic = os.getenv("MNEMONIC","""rough pull enemy remove resource toward army okay slender rapid uphold segment""")
+
+def derive_wallets(coin_name, mnemonic=mnemonic ,numb_derive=3):
+    command = f'php derive -g --mnemonic="{mnemonic}" --cols=address,privkey,pubkey --numderive={numb_derive} --coin={coin_name} --format=json'
+    p =subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) =p.communicate()
+    p.wait()
+    keys = json.loads(output)
+    return keys
+
+coins={"eth":derive_wallets(ETH),
+    "btc-test":derive_wallets(BTCTEST),
+    "btc":derive_wallets(BTC),
+    "ltc":derive_wallets(LTC)}
+
+def priv_key_to_account (coin,priv_key):
+    """
+    This function return the address of a wallet child
+    given its corresponding private key
+    """
+    if coin==ETH:
+        return Account.privateKeyToAccount(priv_key)
+    if coin==BTCTEST:
+        return  PrivateKeyTestnet(priv_key)
+    if coin==BTC:
+        return PrivateKey(priv_key)
+    
